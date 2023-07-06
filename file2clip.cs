1c9e49cb9425fb41db646568b0865b3c90bf171a
Michel Fedde <michel.fedde@gmail.com>
2023-07-06 22:55:30 +0200

    Did some slight refactoring and added console writeline at the end

--- file2clip.cs
+++ file2clip.cs
@@ -4,20 +4,18 @@
 	http://stackoverflow.com/questions/17189010/how-to-copy-cut-a-file-not-the-contents-to-the-clipboard-in-windows-on-the-com
 */
 using System;
-using System.Windows.Forms;
 using System.Collections.Generic;
 using System.Collections.Specialized;
-
+using System.IO;
 using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+using System.Windows;
 
 [assembly: AssemblyTitle("File2Clip")]
 [assembly: AssemblyDescription("copies file to clipboard (as a file)")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("rostok - https://github.com/rostok/")]
 [assembly: AssemblyProduct("File2Clip")]
-[assembly: AssemblyCopyright("Copyright © 2016")]
+[assembly: AssemblyCopyright("Copyright ï¿½ 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: AssemblyVersion("1.0.0.1")]
@@ -27,67 +25,61 @@ namespace File2Clip
 {
     public class App
     {
-    
-    private static bool isConsoleSizeZero { 
-        get{  return 0 == (Console.WindowHeight + Console.WindowWidth); }
-    }
-    public static bool IsOutputRedirected {
-        get { return isConsoleSizeZero && !Console.KeyAvailable; }
-    }
-    public static bool IsInputRedirected {
-        get { return isConsoleSizeZero && Console.KeyAvailable; }
-    }
-
-        [STAThread]
+	    [STAThread]
         static void Main(string[] args)
         {
-			//Console.WriteLine("File2Clip");
-            List<string> list = new List<string>();
+            List<string> files = new List<string>();
 
 	        string line;
 
-			try{
-				bool ka = Console.KeyAvailable;
-				// if this should raise an error we are dealing with redirection
-				// http://stackoverflow.com/questions/12911833/how-to-catch-all-exceptions-in-c-sharp-using-try-and-catch
-				// http://stackoverflow.com/questions/3453220/how-to-detect-if-console-in-stdin-has-been-redirected/3453272#3453272
-				// http://stackoverflow.com/questions/7302544/flushing-system-console-read
-			}
-			catch (Exception e)
-			{
-				//Console.WriteLine("Console.KeyAvailable	PROBLEM");
-    			// stdin 
-                //if (!IsInputRedirected)
-                {
-                	while(!string.IsNullOrEmpty(line = Console.ReadLine())) list.Add(line);
-                }
-    			//Console.WriteLine("Console.In.Peek()	"+ Console.In.Peek());
-			}
-			
+	        if (App.IsRedirected())
+	        {
+		        while (!string.IsNullOrEmpty(line = Console.ReadLine()))
+		        {
+			        files.Add(line);
+		        }
+	        }
 
-            // args
-            foreach (string s in args) {
-            	list.Add(s);
+	        // args
+            foreach (string arg in args) {
+            	files.Add(arg);
             }
 
             StringCollection paths = new StringCollection();
-            foreach (string s in list) 
+            foreach (string file in files) 
             {
-                string t = s.Trim('"');
-                paths.Add( 
-                    System.IO.Path.IsPathRooted(t) ? 
-                      t : 
-                      System.IO.Directory.GetCurrentDirectory() + 
-                        @"\" + t);
+                string t = file.Trim('"');
+                string path = Path.GetFullPath(t);
+                
+                paths.Add(path);
             }
-            if (paths.Count>0)
+            
+            if (paths.Count < 1)
             {
-            	 Clipboard.SetFileDropList(paths);
-            }
-			else
-			{
 				Console.WriteLine("Input is Empty! Provide file paths via STDIN or command line arguments.");
-			}
+				Environment.Exit(87); // 87 is INVALID_PARAMETER, which fits this pretty well. https://learn.microsoft.com/en-us/windows/win32/debug/system-error-codes--0-499-
+            }
+
+            Clipboard.SetFileDropList(paths);
+
+            Console.WriteLine("Files copied to the clipboard");
+        }
+
+        private static bool IsRedirected()
+        {
+	        try{
+		        bool ka = Console.KeyAvailable;
+		        // if this should raise an error we are dealing with redirection
+		        // http://stackoverflow.com/questions/12911833/how-to-catch-all-exceptions-in-c-sharp-using-try-and-catch
+		        // http://stackoverflow.com/questions/3453220/how-to-detect-if-console-in-stdin-has-been-redirected/3453272#3453272
+		        // http://stackoverflow.com/questions/7302544/flushing-system-console-read
+		        return false;
+	        }
+	        catch (Exception e)
+	        {
+		        return true;
+	        }
+
         }
     }
 }
